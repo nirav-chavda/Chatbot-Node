2f2532d0df936f18a404e9deddaf043948d58648
nirav-chavda <niravdchavda@gmail.com>
2018-10-19 18:35:37 +0530

    port error solved

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 var publicPath = path.join(__dirname , "../public");
@@ -8,5 +8,5 @@ var publicPath = path.join(__dirname , "../public");
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-  console.log(`server started on port {PORT}`);
+  console.log(`server started on port ${port}`);
 });
