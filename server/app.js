e618a60322e6699eb64734b4d57c27b791944db1
nirav-chavda <niravdchavda@gmail.com>
2018-10-19 23:49:18 +0530

    Admin Broadcast on new user join

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
+
   console.log('Connected a user');
 
   // socket.emit('newEmail', {
@@ -20,16 +21,35 @@ io.on('connection', (socket) => {
   //   text : 'hey! there'
   // });
 
+  socket.emit('newMessage', {
+    from : 'Admin',
+    text : 'Welcome to chat app',
+    createdAt : new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from : 'Admin',
+    text : 'New User joined',
+    createdAt : new Date().getTime()
+  });
+
   socket.on('disconnect',() => {
     console.log('User Disconnected');
   });
 
   socket.on('createMessage', (message) => {
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from : message.from,
+    //   text : message.text,
+    //   createdAt : new Date().getTime()
+    // });
+
+    socket.broadcast.emit('newMessage', {
       from : message.from,
       text : message.text,
       createdAt : new Date().getTime()
     });
+
   });
 
   // socket.on('createEmail', (email) => {
